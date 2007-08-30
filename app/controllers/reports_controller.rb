4a1148be46923a34ae079cc4fbec198a768dbda5
James Akers <james.akers@dms.myflorida.com>
2007-08-30 20:01:52 +0000

    Yay!

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,78 +1,110 @@
 require 'pdf/writer'
+#require 'pdf/techbook'
+#require 'htmldoc'
 
 class ReportsController < ApplicationController
   before_filter :authenticate 
   layout 'administration', :except => :export_pdf
   
   def index
+    session[:report] = nil
     @type = params[:report_type] rescue ''
+    @use_date = params[:use_date] rescue ''
     @type_select = "<option>Choose Type</option>,
                     <option>Incident</option>,
                     <option>Inmate Count</option>"
-    if @request.post?
-      params[:report][:begin_date] = Date.new(params[:report].delete('begin_date(1i)').to_i, params[:report].delete('begin_date(2i)').to_i, (params[:report].delete('begin_date(3i)')||1).to_i) if params[:report]['begin_date(3i)']
-      params[:report][:end_date] = Date.new(params[:report].delete('end_date(1i)').to_i, params[:report].delete('end_date(2i)').to_i, (params[:report].delete('end_date(3i)')||1).to_i) if params[:report]['end_date(3i)']
-      @use_date = params[:use_date] rescue 0
-      @begin = params[:report][:begin_date]
-      @end = params[:report][:end_date]
-      @mins = params[:report][:mins] rescue ''
-      @incident_type = params[:report][:incident_type_id] rescue ''
-      @search_string = ""
-      
-      if @use_date == 1
-        @search_string += " incident_date >= ? and incident_date <= ? "
-      else
-        @search_string += " incident_date <> ? and incident_date <> ? "
-        @begin = ''
-        @end = ''
-      end
-      unless @mins == "" or @mins == nil
-        @search_string += " and mins = ? "
-      else
-        @search_string += " or mins <> ? "
-      end 
-      unless @incident_type  == "" or @incident_type == nil
-        @search_string += " and incident_type_id = ? "
-      else
-        @search_string += " or incident_type_id <> ? "
-      end
-      
+    @date_select = "<option>No</option>,
+                    <option>Yes</option>"
+    if request.post?
       case @type = params[:report_type]
         
-      when 'Incident'
-        @type_select = "<option>Choose Type</option>, <option selected='true'>Incident</option>, <option>Inmate Count</option>"
-        if session[:access_level] == 'Administrator'
-          @incidents = Incident.find(:all, :conditions => ["" + @search_string + "", @begin, @end, @mins, @incident_type])
-        else
-          @incidents = session[:facility].incidents.find(:all, :conditions => ["" + @search_string + "", @begin, @end, @mins, @incident_type])
-        end
+      when 'Incident'   
+        build_report_incident
         
       when 'Inmate Count'
-        @type_select = "<option>Choose Type</option>, <option>Incident</option>, <option selected='true'>Inmate Count</option>"
+        #build_report_inmate_counts
+        render :text => "Coming Soon", :layout => true
       end
-      
     end
   end
   
   def export_excel
-    @incidents = Incident.find(:all)
-    @response.headers['CONTENT-TYPE'] = 'application/vnd.ms-excel'
-    @response.headers['CONTENT-DISPOSITION'] = 'attachment; filename="Incident Report -' + Time.now.to_s + '.xls"'
+    @incidents = session[:report]
+    response.headers['CONTENT-TYPE'] = 'application/vnd.ms-excel'
+    response.headers['CONTENT-DISPOSITION'] = 'attachment; filename="Incident Report -' + Time.now.to_s + '.xls"'
     render :type => 'application/vnd.ms-excel', :layout => false
   end
   
-  def pdf
-    @response.headers['CONTENT-TYPE'] = 'application/pdf'
-    @response.headers['CONTENT-DISPOSITION'] = 'attachment; filename="Incident Report -' + Time.now.to_s + '.pdf"'
-    render :type => 'application/pdf', :layout => false  
+#  def export_pdf
+#    @incidents = session[:report]
+#    build_pdf
+#    open '/public/pdf/#{@filename}.pdf', :type => 'application/pdf'
+#  end
+#  
+#  def build_pdf
+#    @incidents = session[:report]
+#    @filename = 'incident-' + Time.now.to_s
+#    pdf = PDF::HTMLDoc.new 
+#    pdf.set_option :outfile, "/public/pdf/#{@filename}.pdf"
+#    pdf << (render 'reports/export_excel')
+#    pdf.generate
+#    return @filename
+#  end
+  
+  def build_report_incident
+    @type_select = "<option>Choose Type</option>, <option selected='true'>Incident</option>, <option>Inmate Count</option>"
+
+    @use_date = params[:use_date] rescue ''
+    @mins = params[:report][:mins] rescue ''
+    @incident_type = params[:report][:incident_type_id] rescue ''
+    @facility = params[:report][:facility_id] rescue ''
+    @search_string = ""
+    
+    unless @facility == ''
+      @search_facility = " facility_id = ? and"
+    else
+      @search_facility = " facility_id <> ? and"
+    end
+    
+    if @use_date == 'Yes'
+      @search_string += "incident_date >= ? and incident_date <= ? "
+      @date_select = "<option selected=true>Yes</option>, <option>No</option>"
+      params[:report][:begin_date] = Date.new(params[:report].delete('begin_date(1i)').to_i, params[:report].delete('begin_date(2i)').to_i, (params[:report].delete('begin_date(3i)')||1).to_i) if params[:report]['begin_date(3i)']
+      params[:report][:end_date] = Date.new(params[:report].delete('end_date(1i)').to_i, params[:report].delete('end_date(2i)').to_i, (params[:report].delete('end_date(3i)')||1).to_i) if params[:report]['end_date(3i)'] 
+      @begin = params[:report][:begin_date]
+      @end = params[:report][:end_date]
+    else
+      @search_string += "incident_date <> ? and incident_date <> ? "
+      @date_select = "<option>Yes</option>, <option selected=true>No</option>"
+      @begin = ''
+      @end = ''
+    end
+    
+    unless @mins == '' or @mins == nil
+      @search_string += " and mins = ? "
+    else
+      @search_string += " and mins <> ? "
+    end 
+    unless @incident_type  == '' or @incident_type == nil
+      @search_string += " and incident_type_id = ? "
+    else
+      @search_string += " and incident_type_id <> ? "
+    end
+    
+    if session[:access_level] == 'Administrator'
+      session[:report] = Incident.find(:all, :conditions => [ "" + @search_facility + " " + @search_string + "", @facility, @begin, @end, @mins, @incident_type])
+    else
+      session[:report] = session[:facility].incidents.find(:all, :conditions => ["" + @search_string + "", @begin, @end, @mins, @incident_type])
+    end
   end
   
-  def export_pdf
-    @incidents = Incident.find(:all)
-    pdf = PDF::Writer.new
-    pdf.select_font "Times-Roman"
-    pdf.text((render '/reports/export_excel'), :justification => :left)
-    send_data pdf.render, :filename => "Incident Report -" + Time.now.to_s + ".pdf",
-    :type => "application/pdf"
+  def build_report_inmate_counts  
+    
+    @type_select = "<option>Choose Type</option>, <option>Incident</option>, <option selected='true'>Inmate Count</option>"
+    if session[:access_level] == 'Administrator'
+      session[:report] = InmateCount.find(:all, :conditions => [ "" + @search_facility + " " + @search_string + "", @facility])
+    else
+      session[:report] = session[:facility].inmate_counts.find(:all, :conditions => ["" + @search_string + "", @begin, @end])
+    end
   end
 end

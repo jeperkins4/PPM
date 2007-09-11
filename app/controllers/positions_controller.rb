96e40a5e604bdec6b44c8b797583e3a3049d3b89
Anthony Roeder <anthony.roeder@dms.myflorida.com>
2007-09-11 20:29:05 +0000

    position_hist updates

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -63,20 +63,31 @@ class PositionsController < ApplicationController
   # PUT /positions/1
   # PUT /positions/1.xml
   def update
-    @position = Position.find(params[:id])
-    @salary = params[:position][:salary]
-    if @position.salary.to_i = @salary.to_i
-       bang!
-    end
-
+    @position = Position.find(params[:id])    
     respond_to do |format|
-      if @position.update_attributes(params[:position])
+      
+      if @position.salary.to_i != (params[:position][:salary]).to_i
+        session[:position_history] = @position
+        @position_history = PositionHist.new( :title             => session[:position_history].title, 
+                                              :position_type_id  => session[:position_history].position_type_id,
+                                              :salary            => session[:position_history].salary,
+                                              :description       => session[:position_history].description, 
+                                              :facility_id       => session[:position_history].facility_id, 
+                                              :create_date       => Time.now )
+        @position_history.save
+        
+        @position.update_attributes(params[:position])
         flash[:notice] = 'Position was successfully updated.'
         format.html { redirect_to position_url(@position) }
-        format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @position.errors.to_xml }
+        if @position.update_attributes(params[:position])
+          flash[:notice] = 'Position was successfully updated.'
+          format.html { redirect_to position_url(@position) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @position.errors.to_xml }
+        end
       end
     end
   end

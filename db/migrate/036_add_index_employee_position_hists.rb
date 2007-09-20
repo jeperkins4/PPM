a0c65f4d3b511f7d913b89391229015063db39c5
James Akers <james.akers@dms.myflorida.com>
2007-09-20 19:19:52 +0000

    Yay

--- db/migrate/036_add_index_employee_position_hists.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexEmployeePositionHist < ActiveRecord::Migration
-  def self.up
-    add_index :position_number_id
-  end
-  
-  def self.down
-    remove_index :position_number_id
-  end
-end

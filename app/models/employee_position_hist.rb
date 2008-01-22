6dbebc536bef2026138dd652b8a4b86d9d88df95
Anthony Roeder <anthony.roeder@dms.myflorida.com>
2008-01-22 17:38:43 +0000

    added database validation

--- app/models/employee_position_hist.rb
+++ app/models/employee_position_hist.rb
@@ -1,6 +1,9 @@
 class EmployeePositionHist < ActiveRecord::Base
   
-  validates_presence_of :position_number_id, :employee_id, :start_date, :end_date
+  validates_presence_of :position_number_id
+  validates_presence_of :employee_id
+  validates_presence_of :start_date
+  validates_presence_of :end_date  
  
   belongs_to :position_number
   belongs_to :employee

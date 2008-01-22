1b4053a0d065e34c928a4c97f3ed0edd5660b045
Anthony Roeder <anthony.roeder@dms.myflorida.com>
2008-01-22 17:35:13 +0000

    added database validation

--- app/models/employee_position_hist.rb
+++ app/models/employee_position_hist.rb
@@ -1,6 +1,10 @@
 class EmployeePositionHist < ActiveRecord::Base
+  
+  validates_presence_of :position_number_id, :employee_id, :start_date, :end_date
  
   belongs_to :position_number
   belongs_to :employee
+  
+  force_uppercase :include_text => true
 
 end

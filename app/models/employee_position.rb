af0a2186eea55dff4b2bac4826dfa3ee9469bd4c
Anthony Roeder <anthony.roeder@dms.myflorida.com>
2008-01-29 19:55:09 +0000

    update to employee_position

--- app/models/employee_position.rb
+++ app/models/employee_position.rb
@@ -6,4 +6,14 @@ class EmployeePosition < ActiveRecord::Base
   validates_uniqueness_of :employee_id, :message => "is already assigned to a Position"
   validates_uniqueness_of :position_number_id, :message => "is already assigned to an employee"
   validates_presence_of :start_date
- end
+  force_uppercase :include_text => true
+    
+  def self.available_positions(facility_id)
+    EmployeePosition.find(:all, 
+                          :select => 'ep.id as id, ep.position_number_id, ep.employee_id, ep.start_date, ep.end_date', 
+                          :order=>'p.title',
+                          :from=>'employee_positions ep, position_numbers pn, positions p, facilities f',
+                          :conditions=>['ep.position_number_id = pn.id and pn.position_id = p.id and p.facility_id = f.id
+                          and f.id = ?', facility_id])
+  end
+end

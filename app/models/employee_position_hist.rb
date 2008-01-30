a8abbd4904ada71d9315d1b8b16f53876a54e070
Anthony Roeder <anthony.roeder@dms.myflorida.com>
2008-01-30 01:52:29 +0000

    added def.self

--- app/models/employee_position_hist.rb
+++ app/models/employee_position_hist.rb
@@ -1,13 +1,22 @@
 class EmployeePositionHist < ActiveRecord::Base
+  belongs_to :position_number
+  belongs_to :employee
   
   validates_presence_of :position_number_id
   validates_presence_of :employee_id
   validates_presence_of :start_date
   validates_presence_of :end_date  
  
-  belongs_to :position_number
-  belongs_to :employee
   
   force_uppercase :include_text => true
+  
+  def self.available_positions(facility_id)
+    EmployeePosition.find(:all, 
+      :select => 'ep.id as id, ep.position_number_id, ep.employee_id, ep.start_date, ep.end_date', 
+      :order=>'p.title',
+      :from=>'employee_positions ep, position_numbers pn, positions p, facilities f',
+      :conditions=>['ep.position_number_id = pn.id and pn.position_id = p.id and p.facility_id = f.id
+                          and f.id = ?', facility_id])
+  end
 
 end

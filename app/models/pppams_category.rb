01695cf497b1b76304526725c41b4b48cfbfaaee
Joe Martinez <joe.martinez@dms.myflorida.com>
2008-04-02 13:45:28 +0000

    Added CRUD unit tests for PppamsCategory and PppamsIndicator, set dev environment to use local db

--- app/models/pppams_category.rb
+++ app/models/pppams_category.rb
@@ -1,3 +1,4 @@
 class PppamsCategory < ActiveRecord::Base
     belongs_to :facility
+    validates_presence_of [:name,:description,:facility_id]
 end
--- app/models/pppams_indicator.rb
+++ app/models/pppams_indicator.rb
@@ -1,3 +1,4 @@
 class PppamsIndicator < ActiveRecord::Base
       belongs_to :pppams_category
+      validates_presence_of [:reference,:description,:pppams_category_id,:frequency,:indicator_rating,:max_score,:start_month]
 end

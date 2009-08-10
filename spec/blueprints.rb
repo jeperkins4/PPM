f76490ac8e5d69532e91672345f3e8dabe22f584
Bernie Telles <btelles@gmail.com>
2009-08-10 16:43:29 -0400

    add rspec 1.1.12 (latest version compatible with rails 1.2.3) issue #138

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -1,42 +1,109 @@
 require 'machinist/active_record'
 require 'sham'
+require 'faker'
 
 Sham.define do
-  full_company_name = Faker::Compary.name
-  company { full_company_name}
-  company_short {full_name[0..3]}
+  company { Faker::Company.name}
+  company_short {company[0..10]}
   address { Faker::Address.street_address}
   city    { Faker::Address.city}
   state   { Faker::Address.us_state}
   zip     { Faker::Address.zip_code }
   phone   { Faker::PhoneNumber.phone_number}
   name    { Faker::Name.name}
-  pos_title %w{WARDEN MANAGER DENTIST}.rand
-  paragraphs { Faker::Lorem.paragraphs(3)}
+  description { Faker::Lorem.paragraphs(3)}
+  user_name { Faker::Internet.user_name}
+  first_name {Faker::Name.first_name}
+  last_name { Faker::Name.last_name}
 end
 
 Facility.blueprint do
   facility         { Sham.company }
-  short_name       { Sham.company_short }
   address1         {Sham.address}
-  city             { Sham.city}
   state            { Sham.state}
-  zip              { Sham.zip }
-  phone            { Sham.phone}
   warden           { Sham.name}
   contract_monitor { Sham.name}
+  zip 
+  phone 
+  city
+end
+Facility.blueprint(:with_position_number) do
+  
+end
+EmployeePosition.blueprint do
+  position_number { PositionNumber.make}
+  employee        { Employee.make}
+  start_date      { (2..8).to_a.rand.years.ago }
+end
+
+Employee.blueprint do
+  first_name
+  last_name
+  tea_status { %w{certified na facility academy pending registered}.rand }
 end
 
 Position.blueprint do
-  title          { Sham.pos_title}
-  salary_decimal { Faker.numerify((5..8).to_a.rand.times.map {|a| '#'}.join('')+'.##')}
-  description    { Sham.paragraphs }
+  title          { %w{WARDEN MANAGER DENTIST}.rand }
+  salary         { Faker.numerify((5..8).to_a.rand.times.map {|a| '#'}.join('')+'.##')}
+  description    
+  position_type       { PositionType.make }
+end
+
+PositionNumber.blueprint do
+  position_num { Faker.numerify('#######')}
+  position { Position.make}
+  position_type { %w{none iwtf contract}.rand}
 end
 
 PositionType.blueprint do
   position_type  { ['Security', 'Non-Security'].rand }
-  description    { Sham.paragraphs }
+  description    
   deductable     { (0..1).to_a.rand }
   deduction_days { (10..120).to_a.rand }
-  
+  facility       { Facility.make }
 end
+
+PositionType.blueprint(:with_facility) do
+  position_type  { ['Security', 'Non-Security'].rand }
+  description    
+  deductable     { (0..1).to_a.rand }
+  deduction_days { (10..120).to_a.rand }
+  facility       { Facility.make }
+end
+
+AccessLevel.blueprint do
+  id { 2 }
+  access_level { Sham.word }
+end
+
+AccessLevel.blueprint(:administrator) do
+  id { 1 }
+  access_level { 'Administrator' }
+  description  {'Replace Text'}
+end
+
+UserType.blueprint do
+  id { 2 }
+  user_type {'some type'}
+  access_level { AccessLevel.make }
+end
+UserType.blueprint(:administrator) do
+  user_type    { 'Administrator'}
+  description  { 'Full rights'}
+  access_level { AccessLevel.make(:administrator)}
+end
+
+User.blueprint do
+  name { Sham.user_name}
+  password  { 'faker' }
+  password_confirmation { 'faker' }
+end
+
+User.blueprint(:administrator) do
+  name { Sham.user_name}
+  password  { 'faker' }
+  password_confirmation { password }
+  user_type { UserType.make(:administrator) }
+end
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,24 @@
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
-require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
-require 'spec/autorun'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'spec'
 require 'spec/rails'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-# Require machinist blueprints
 require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
+
+  include AuthenticatedTestHelper
+
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
   
-  # Reset machinist shams before each test
+  # Reset machinist tests
   config.before(:all)    { Sham.reset(:before_all)  }
   config.before(:each)   { Sham.reset(:before_each) }
 
@@ -53,6 +51,6 @@ Spec::Runner.configure do |config|
   # config.mock_with :rr
   #
   # == Notes
-  #
+  # 
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end

6ec4839f2ddd0767381eaf7344df2ec8a7870cff
Bernie Telles <bernardo.telles@dms.myflorida.com>
2009-12-28 12:55:53 -0500

    Add image actions test to overarll indicators.

--- spec/views/pppams_indicator_base_refs/index.html.erb_spec.rb
+++ spec/views/pppams_indicator_base_refs/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ describe "/pppams_indicator_base_refs/index.html.erb" do
   include PppamsIndicatorBaseRefsHelper
 
   before(:each) do
-    assigns[:pppams_indicator_base_refs] = [
+    assigns[:pppams_indicator_base_refs] = WillPaginate::Collection.new(1,10).replace([
       stub_model(PppamsIndicatorBaseRef,
         :question => "value for question",
         :pppams_category_base_ref_id => 1
@@ -13,11 +13,15 @@ describe "/pppams_indicator_base_refs/index.html.erb" do
         :question => "value for question",
         :pppams_category_base_ref_id => 1
       )
-    ]
+    ])
   end
 
   it "renders a list of pppams_indicator_base_refs" do
     render
     response.should have_tag("tr>td", "value for question".to_s, 2)
+  end
+  it  'should use pictures for actions' do
+    render
+    response.should have_tag('a[href*=/pppams_indicator_base_refs] img')
   end
 end

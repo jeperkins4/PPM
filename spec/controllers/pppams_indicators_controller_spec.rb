ad4d2eccab374f17751f60d00111b468860f45cd
Bernie Telles <bernardo.telles@dms.myflorida.com>
2009-12-29 14:33:19 -0500

    Fix references link and update more overall/global references.

--- spec/controllers/pppams_indicators_controller_spec.rb
+++ spec/controllers/pppams_indicators_controller_spec.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PppamsIndicatorsController do
-  it 'should handle  pppams_indicators that have been deleted/inactivated gracefully'
+  it 'should handle  pppams_indicators that have been deleted/inactivated gracefully' do
+    
+  end
 end

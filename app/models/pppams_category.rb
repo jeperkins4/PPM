fc66cbd0aba25eb15f534560dd138acb169526ae
Bernie Telles <bernardo.telles@dms.myflorida.com>
2010-01-15 10:24:13 -0500

    Remove pppams_category model and controller. there are remnants of these files still in the application.

--- app/models/pppams_category.rb
+++
@@ -1,7 +0,0 @@
-class PppamsCategory < ActiveRecord::Base
-
-    belongs_to :facility
-    belongs_to :pppams_category_base_ref
-    has_many :pppams_indicators
-    validates_presence_of [:name,:facility_id]
-end
--- spec/controllers/pppams_categories_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe PppamsCategoriesController do
-  it 'should handle  pppams_indicators that have been deleted/inactivated gracefully'
-end

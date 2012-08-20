f02c31c381916db826fe87576d9eb9be1c485ee5
jeperkins4 <jeperkins4@gmail.com>
2012-08-20 13:18:59 -0400

    Changes to port app successfully

--- app/models/accountability_logs.rb
+++ app/models/accountability_logs.rb
@@ -1,12 +1,3 @@
 class AccountabilityLogs < ActiveRecord::Base
   belongs_to :facility
-
-  def self.filtered_logs(start_date, end_date)
-    logs = []
-    date_range = (start_date..end_date).map{|d|{:month => d.month, :year => d.year}}.uniq
-    date_range.each do |month_year|
-      logs |= AccountabilityLogs.find(:all, :conditions => {:log_month => month_year[:month], :log_year => month_year[:year]})
-    end
-    return logs
-  end
 end

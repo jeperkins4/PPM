94e3953a05dabd4e55f922238b7a8a8e9fb3ee1e
jeperkins4 <jeperkins4@gmail.com>
2012-08-22 11:38:53 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrivatePrisons::Application.config.secret_token = '1fb0ea419fd78f15addbcd19ebeed93cef1b6053480cab6cd9e2677d6d75b0d7b1fd46c586e4adc9db18c429eaf78ac2b60607a46d134b544fb18da03a874fb8'

3c7b13f74834ee0705588166638d157699746cb0
jeperkins4 <jeperkins4@gmail.com>
2012-09-19 13:20:18 -0400

    Cleanup

--- vendor/plugins/text_area_with_status/init.rb
+++
@@ -1,3 +0,0 @@
-ActionView::Helpers::InstanceTag.send :include, TextAreaWithStatus::InstanceTagExt
-ActionView::Helpers::FormTagHelper.send :include, TextAreaWithStatus::FormTagHelperExt
-ActionView::Helpers::AssetTagHelper.register_javascript_include_default('limit_chars')
